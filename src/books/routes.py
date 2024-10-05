2bac9cbc112744ccc4d8fed33ed1d6f00d15da3e
Ikechukwu Okoro <ikeshegs@gmail.com>
2024-10-05 16:07:35 +0100

    create route for getting all books

--- src/books/routes.py
+++ src/books/routes.py
@@ -1,8 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status, Depends
+from fastapi.exceptions import HTTPException
+from typing import List
+from sqlmodel.ext.asyncio.session import AsyncSession
+
+from src.books.schemas import Book, BookUpdateModel
+from src.books.models import Book
+from src.books.service import BookService
+from src.db.main import get_session
 
 book_router = APIRouter()
+book_service = BookService()
 
 @book_router.get("/")
-async def get_all_books() -> dict:
-    pass
-    # return books
+async def get_all_books(session: AsyncSession = Depends(get_session)):
+    books = book_service.get_all_books(session)
+    return books
