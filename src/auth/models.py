5309b46f5c787452c643b3b978811d2a1fbffe64
Ikechukwu Okoro <ikeshegs@gmail.com>
2024-10-07 18:36:16 +0100

    create new_user route

--- src/auth/models.py
+++ src/auth/models.py
@@ -19,7 +19,8 @@ class User(SQLModel, table = True):
     email: str
     first_name: str
     last_name: str
-    is_verified: bool = False
+    is_verified: bool = Field(default=False)
+    password_hash: str = Field(exclude=True)
     created_at: datetime = Field(
         sa_column=Column(
             pg.TIMESTAMP,
