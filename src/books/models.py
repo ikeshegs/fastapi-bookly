db8e51fe71d2dcfd28d3f0f14f0e5f4abf0889b9
Ikechukwu Okoro <ikeshegs@gmail.com>
2024-10-11 13:35:06 +0100

    add foreign key & implement getting all book from a user

--- src/books/models.py
+++ src/books/models.py
@@ -2,6 +2,7 @@ from sqlmodel import SQLModel, Field, Column
 import sqlalchemy.dialects.postgresql as pg
 from datetime import date, datetime
 import uuid
+from typing import Optional
 
 
 class Book(SQLModel, table=True):
@@ -22,6 +23,7 @@ class Book(SQLModel, table=True):
     published_date: date
     page_count: int
     language: str
+    user_uid: Optional[uuid.UUID] = Field(default=None, foreign_key="users.uid")
     created_at: datetime = Field(
         sa_column=Column(
             pg.TIMESTAMP,
--- src/books/service.py
+++ src/books/service.py
@@ -16,6 +16,18 @@ class BookService:
         return result.all()
     
 
+    async def get_all_user_books(self, user_uid: str, session: AsyncSession):
+        statement = (
+            select(Book)
+            .where(Book.user_uid == user_uid)
+            .order_by(desc(Book.created_at))
+        )
+
+        result = await session.exec(statement)
+
+        return result.all()
+    
+
     async def get_book(self, book_uid: str, session: AsyncSession):
         statement = select(Book).where(Book.uid == book_uid)
 
@@ -26,7 +38,9 @@ class BookService:
         return book if book is not None else None 
     
 
-    async def create_book(self, book_data: BookCreateModel, session: AsyncSession):
+    async def create_book(
+        self, book_data: BookCreateModel, user_uid: str, session: AsyncSession):
+
         book_data_dict = book_data.model_dump()
 
         new_book = Book(
@@ -34,6 +48,8 @@ class BookService:
         )
 
         new_book.published_date = datetime.strptime(book_data_dict["published_date"], "%Y-%m-%d")
+
+        new_book.user_uid = user_uid
 
         session.add(new_book)
 
