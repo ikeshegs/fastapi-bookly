a33f866758d447cf4c33c8e74274c33ec94c1ac9
Ikechukwu Okoro <ikeshegs@gmail.com>
2024-10-11 11:48:29 +0100

    create get current user route

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -8,7 +8,7 @@ from .schemas import UserCreateModel, UserModel, UserLoginModel
 from .service import UserService
 from src.db.main import get_session
 from .utils import create_access_token, verify_password
-from .dependencies import RefreshTokenBearer, AccessTokenBearer
+from .dependencies import RefreshTokenBearer, AccessTokenBearer, get_current_user
 from src.db.redis import add_jti_to_blocklist
 
 auth_router = APIRouter()
@@ -106,6 +106,12 @@ async def get_new_access_token(token_details: dict = Depends(RefreshTokenBearer(
         status_code=status.HTTP_400_BAD_REQUEST, 
         details="Invalid or Expiry Token"
     )
+
+
+@auth_router.get("/me")
+async def get_current_user(user = Depends(get_current_user)):
+    
+    return user
 
 
 @auth_router.post("/logout")
